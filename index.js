ad53ffa5421af3ce7e117d5c8218cc7cb2afbac1
Muhammad Afaq <iafaqkhattak@gmail.com>
2023-11-10 03:08:22 +0500

    Update and Delete Products

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// import these libraries
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
@@ -9,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // use for json files
 app.use(express.json());
 
-// database
+// database connections
 mongoose
   .connect("mongodb://localhost:27017/ecommerce")
   .then(() => {
@@ -18,24 +19,88 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-//Schema
+
+//Schema for database
 const productSchema = mongoose.Schema({
   name: String,
   description: String,
   price: Number,
 });
 
-//models ---like collections
+//models ---like collections for database
 const productModels = mongoose.model("Product", productSchema);
 
-//Create product
+/*Create product
+response with status 201 for create products */
+
 app.post("/api/v1/product/new", async (req, res) => {
   const product = await productModels.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+});
 
-  //response with status
+/*Read Product
+use find()
+status 200 for read product */
+app.get("/api/v1/products", async (req, res) => {
+  const products = await productModels.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
+});
+
+/*update product,
+will use Let key word,
+use findbyid(),
+use findbyidAndUpdate (),
+//useFindAndModify = true,
+runValidators = true */
+
+app.put("/api/v1/products/:id", async (req, res) => {
+  let product = await productModels.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+  product = await productModels.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    useFindAndModify: false,
+    runValidators: true,
+  });
+
   res.status(200).json({
     success: true,
     product,
+  });
+});
+
+/* Delete a Product
+First Find a product using findbyID
+use deleteOne method to remove product
+response the data
+
+ */
+app.delete("/api/v1/product/:id", async (req, res) => {
+  const product = await productModels.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+
+  await product.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "Product Deleted Successfully",
   });
 });
 
