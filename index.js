7013e3a48803b7f52f43869b3b1e5527a260b4bb
Muhammad Afaq <iafaqkhattak@gmail.com>
2023-11-10 01:23:33 +0500

    Connect with database and create product api

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+const bodyParser = require("body-parser");
 
-app.listen(port, () => {
-  console.log(``);
+//use body parser when req data from front-end
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// use for json files
+app.use(express.json());
+
+// database
+mongoose
+  .connect("mongodb://localhost:27017/ecommerce")
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+//Schema
+const productSchema = mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+});
+
+//models ---like collections
+const productModels = mongoose.model("Product", productSchema);
+
+//Create product
+app.post("/api/v1/product/new", async (req, res) => {
+  const product = await productModels.create(req.body);
+
+  //response with status
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+//server
+app.listen(4500, () => {
+  console.log("Server is working on http://localhost:4500");
 });
